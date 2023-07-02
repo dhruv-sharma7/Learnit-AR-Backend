78d7c6a3fb3a4df1bc2103ecbff5b584f6114406
dhruv-sharma7 <dhruv.722202@gmail.com>
2023-07-03 00:29:48 +0530

    AR-Backend

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+app.use(express.static('A')); 
+
+// Enable CORS for all routes
+app.use(cors());
+
+// Handling GET request
+app.get('/', (req, res) => {
+    res.send('A simple Node App is running on thi');
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
+});
